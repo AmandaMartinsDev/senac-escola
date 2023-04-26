bd800f7b85d6778cf169d556312638c51fc8b781
Amanda Martins Xavier <hi@amandamartins.dev>
2023-04-26 18:34:13 -0300

    :art: Formatação de código com Prettier e ESlint

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { Inkline, components } from '@inkline/inkline';
-import '@inkline/inkline/inkline.scss';
+import { Inkline, components } from "@inkline/inkline";
+import "@inkline/inkline/inkline.scss";
 
 import "./styles/global.scss";
 
 const app = createApp(App);
 
 app.use(Inkline, {
-    colorMode: 'light',
-    components
+  colorMode: "light",
+  components,
 });
-    
-app.mount('#app');
+
+app.mount("#app");
