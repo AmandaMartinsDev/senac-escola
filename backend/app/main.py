eee7eb29b5ab97e06ce7445f04cf43c241065392
Amanda Martins Xavier <hi@amandamartins.dev>
2023-05-14 17:02:03 -0300

    :sparkles: Criação da exclusão e atualização de pessoas usuárias

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ def get_db():
         db.close()
 
 
-@app.post('/users/', response_model=schemas.User)
+@app.post('/users/', response_model=schemas.UserRead)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_email=user.user_email)
     if db_user:
@@ -30,15 +30,33 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get('/users/', response_model=list[schemas.User])
+@app.get('/users/', response_model=list[schemas.UserRead])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get('/users/{user_email}', response_model=schemas.User)
+@app.get('/users/{user_email}', response_model=schemas.UserRead)
 def read_user(user_email: str, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_email=user_email)
     if db_user is None:
         raise HTTPException(status_code=404, detail='Usuário não encontrado')
     return db_user
+
+
+@app.patch('/users/{user_email}', response_model=schemas.UserRead)
+def update_hero(
+    user_email: str, user: schemas.UserUpdate, db: Session = Depends(get_db)
+):
+    db_user = crud.get_user(db, user_email=user_email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='Usuário não encontrado')
+    return crud.update_user(db=db, db_user=db_user, user=user)
+
+
+@app.delete('/users/{user_email}')
+def delete_user(user_email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_email=user_email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='Usuário não encontrado')
+    return crud.delete_user(db=db, db_user=db_user)
